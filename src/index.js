dc4de12a43541e871efca8283665ebfd9b8189f2
Cameron <camcreel@gmail.com>
2021-05-27 18:32:44 -0400

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,12 +1,31 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+} from "react-router-dom";
 
-function Home() {
-  return (
-    <div>
-      <h1>Home!</h1>
-    </div>
-  );
-}
+import NavBar from "./components/NavBar"
+import Home from "./components/Home"
+import About from "./components/About"
+import Login from "./components/Login"
 
-ReactDOM.render(<Home />, document.getElementById("root"));
+
+ReactDOM.render(
+  <Router>
+    <NavBar />
+    <Switch>
+      <Route exact path="/about">
+        <About />
+      </Route>
+      <Route exact path="/login">
+        <Login />
+      </Route>
+      <Route exact path="/">
+        <Home />
+      </Route>
+    </Switch>
+  </Router>,
+  document.getElementById("root")
+); 
